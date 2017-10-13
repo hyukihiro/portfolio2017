86d0367f8095255ad78e21184e3c4e6628bdb480
Yukihiro Hoshide <hoshideyukihiro@gmail.com>
2017-10-13 15:51:03 +0900

    [update] showing time and weather api

--- src/assets/js/_module/_meta.js
+++ src/assets/js/_module/_meta.js
@@ -5,12 +5,20 @@ export function date() {
   let hour = time.getHours();
   let minute = time.getMinutes();
 
+  if ( minute < 10 ) {
+    minute = '0' + minute;    
+  }
+
+  if ( hour < 10 ) {
+    hour = '0' + hour;    
+  }
+
   $('.meta__item .minute').html(hour);
   $('.meta__item .second').html(minute);
 
-  // setInterval(function() {
-  //   date();
-  // }, 1000)
+  setInterval(function() {
+    date();
+  }, 60000)
 }
 
 export function weather() {
@@ -19,14 +27,12 @@ export function weather() {
     url: 'http://api.openweathermap.org/data/2.5/weather?id=1853909&units=metric&appid=2f890cb41f31cbbba489407e0936a6d5',
     dataType: 'json',
     success: function(response) {
-      console.dir(response.weather);
-        $('.sns').text(response);
+      let weather = response.weather[0].main;
+      $('.meta__cat--weather .meta__item').text(weather);
     },
     error: function() {
-        alert('えらぁ');
     },
     complete: function() {
-        alert('こんぷりーと');
     }
   });
 }
